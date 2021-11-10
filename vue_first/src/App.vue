2536b96c9583cc0272fa1e9a223de266778e25f5
ItzArima <skaypaprilia@gmail.com>
2021-11-10 18:37:53 +0100

    Non sapevo come pushare tutti i file quindi pusho solo src con esercizio completato

---
+++ vue_first/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+    <Jumbotron/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Jumbotron from "./components/Jumbotron.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Jumbotron,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_first/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>La mia prima app con Vue CLI!</h1>
+    <h4>Immaginiamo che questo logo qui sotto sia un jumbotron</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
